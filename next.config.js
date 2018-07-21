5b884edfcbf9210c2a83e9465951a0935f3f161e
Carlos Azuaje <carlosjazzc1@gmail.com>
2018-07-21 17:21:52 -0400

    feat(pwa): add sw and manifest json
    Signed-off-by: Carlos Azuaje <carlosjazzc1@gmail.com>

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withOffline = require('next-offline')
+
+module.exports = withOffline({
+    webpack: (config) => {
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+            fs: 'empty'
+        }
+
+        return config
+    }
+})
